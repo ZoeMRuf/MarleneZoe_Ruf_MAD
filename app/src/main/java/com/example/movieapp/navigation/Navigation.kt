00b07ad611f4d7dd4092ddc484fc34399d794a97
ZoeMRuf <92019231+ZoeMRuf@users.noreply.github.com>
2023-03-15 15:12:59 +0100

    started ld3

---
+++ app/src/main/java/com/example/movieapp/navigation/Navigation.kt
@@ -0,0 +1,15 @@
+package com.example.movieapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.movieapp.screens.HomeScreen
+
+@Composable
+fun Navigation (){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "homescreen"){
+        composable("homescreen"){ HomeScreen()}
+    }
+}
